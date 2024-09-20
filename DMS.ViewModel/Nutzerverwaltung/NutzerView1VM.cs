2a655b0443991a1e9697299dd0b155efed3c7f03
Mustafa Shahin <mshahin@green-solutions.net>
2024-09-20 15:49:32 +0200

    feature/set user active and admin

--- DMS.ViewModel/Nutzerverwaltung/NutzerView1VM.cs
+++ DMS.ViewModel/Nutzerverwaltung/NutzerView1VM.cs
@@ -2,59 +2,64 @@
 using DMS.Model;
 using DMS.Service;
 
-namespace DMS.ViewModel.Nutzerverwaltung;
-
-public class NutzerView1VM : ViewModelBase, INutzerView1VM
+namespace DMS.ViewModel.Nutzerverwaltung
 {
-    private BenutzerService m_benutzerService;
-
-    private Benutzer m_currentUser;
-    private ObservableCollection<Benutzer> m_benutzerCollection;
-
-    public Benutzer CurrentUser
+    public class NutzerView1VM : ViewModelBase, INutzerView1VM
     {
-        get => m_currentUser;
-        set
+        private BenutzerService m_benutzerService;
+
+        private Benutzer m_currentUser;
+        private ObservableCollection<Benutzer> m_benutzerCollection;
+
+        public Benutzer CurrentUser
         {
-            if (Equals(value, m_currentUser)) return;
-            m_currentUser = value;
-            OnPropertyChanged();
+            get => m_currentUser;
+            set
+            {
+                if (Equals(value, m_currentUser)) return;
+                m_currentUser = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public ObservableCollection<Benutzer> BenutzerCollection
-    {
-        get => m_benutzerCollection;
-        set
+        public ObservableCollection<Benutzer> BenutzerCollection
         {
-            if (Equals(value, m_benutzerCollection)) return;
-            m_benutzerCollection = value;
-            OnPropertyChanged();
+            get => m_benutzerCollection;
+            set
+            {
+                if (Equals(value, m_benutzerCollection)) return;
+                m_benutzerCollection = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public DelegateCommand SaveBenutzerCommand { get; set; }
+        public DelegateCommand SaveBenutzerCommand { get; set; }
 
-    public NutzerView1VM(BenutzerService benutzerService)
-    {
-        m_benutzerService = benutzerService;
-
-        SaveBenutzerCommand = new DelegateCommand(SaveChanges);
-    }
-
-    public void Init(Benutzer currentUser)
-    {
-        CurrentUser = currentUser;
-        BenutzerCollection = new ObservableCollection<Benutzer>(m_benutzerService.GetAllUsers());
-    }
-
-    private void SaveChanges(object? o)
-    {
-        Benutzer benutzer = (Benutzer)o;
-        if(benutzer != null)
+        public NutzerView1VM(BenutzerService benutzerService)
         {
-            m_benutzerService.SaveChanges(benutzer);
-            Init(CurrentUser);
+            m_benutzerService = benutzerService;
+
+            SaveBenutzerCommand = new DelegateCommand(SaveChanges);
+        }
+
+        public void Init(Benutzer currentUser)
+        {
+            CurrentUser = currentUser;
+
+            BenutzerCollection = new ObservableCollection<Benutzer>(
+       m_benutzerService.GetAllUsers()
+       .OrderByDescending(b => b.IsAdmin)    
+       .ThenByDescending(b => b.IsActive));
+        }
+
+        private void SaveChanges(object? o)
+        {
+            Benutzer benutzer = (Benutzer)o;
+            if (benutzer != null)
+            {
+                m_benutzerService.SaveChanges(benutzer);
+                Init(CurrentUser);
+            }
         }
     }
 }
