5ad72d27c93cc6e69cfa348dd3e14f61843d45bc
Mustafa Shahin <mustafashahin988@gmail.com>
2024-09-25 00:50:29 +0200

    name und password should not be empty

--- DMS.Model/Benutzer.cs
+++ DMS.Model/Benutzer.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DMS.Model;
-
-public class Benutzer
+namespace DMS.Model
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    [MinLength(8, ErrorMessage = "Das Passwort muss mindestens 8 Zeichen lang sein.")]
-    public string Passwort { get; set; }
-    public bool IsAdmin { get; set; }
-    public bool IsActive { get; set; }
+    public class Benutzer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Der Name darf nicht leer sein.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Das Passwort darf nicht leer sein.")]
+        [MinLength(8, ErrorMessage = "Das Passwort muss mindestens 8 Zeichen lang sein.")]
+        public string Passwort { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+    }
 }
