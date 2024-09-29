b65a6a9251eb0e7e53e74576a1e36d0d775db814
Mustafa Shahin <mshahin@green-solutions.net>
2024-09-29 02:34:07 +0200

    Logic of creating folderrs

--- DMS.ViewModel/Ordneruebersicht/OrdnerFrameVM.cs
+++ DMS.ViewModel/Ordneruebersicht/OrdnerFrameVM.cs
@@ -1,33 +1,73 @@
 ﻿using DMS.Model;
+using DMS.Service;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using ViewModel.Interface;
 
-namespace DMS.ViewModel.Ordneruebersicht;
-
-public class OrdnerFrameVM : ViewModelBase, IOrdnerFrameVM
+namespace DMS.ViewModel.Ordneruebersicht
 {
-    private IOrdnerView1VM m_ordnerView1VM;
-
-    private IViewModelBase m_currentView;
-
-    public IViewModelBase CurrentView
+    public class OrdnerFrameVM : ViewModelBase, IOrdnerFrameVM
     {
-        get => m_currentView;
-        set
+        private readonly IOrdnerView1VM m_ordnerView1VM;
+        private readonly OrdnerService m_ordnerService;
+        private IViewModelBase m_currentView;
+
+        public IViewModelBase CurrentView
         {
-            if (Equals(value, m_currentView)) return;
-            m_currentView = value;
-            OnPropertyChanged();
+            get => m_currentView;
+            set
+            {
+                if (Equals(value, m_currentView)) return;
+                m_currentView = value;
+                OnPropertyChanged();
+            }
         }
-    }
 
-    public OrdnerFrameVM(IOrdnerView1VM ordnerView1VM)
-    {
-        m_ordnerView1VM = ordnerView1VM;
-    }
+        public ObservableCollection<Ordner> OrdnerCollection { get; set; }
+        public event EventHandler<Ordner> FolderCreated;
 
-    public void Init(Benutzer currentUser)
-    {
-        m_ordnerView1VM.Init(currentUser);
-        CurrentView = m_ordnerView1VM;
+        public DelegateCommand CreateFolderCommand { get; set; }
+
+        public OrdnerFrameVM(IOrdnerView1VM ordnerView1VM, OrdnerService ordnerService)
+        {
+            m_ordnerService = ordnerService;
+            m_ordnerView1VM = ordnerView1VM;
+            OrdnerCollection = [];
+
+            CreateFolderCommand = new DelegateCommand(OnCreateFolder);
+            LoadFoldersAsync();
+        }
+
+        public void Init(Benutzer currentUser)
+        {
+            m_ordnerView1VM.Init(currentUser);
+            CurrentView = m_ordnerView1VM;
+        }
+
+        private async void OnCreateFolder(object? o)
+        {
+            var newFolder = await m_ordnerService.CreateFolderAsync(); 
+            OrdnerCollection.Add(newFolder);  
+            FolderCreated?.Invoke(this, newFolder); 
+        }
+
+        // Load folders by calling OrdnerService
+        private async void LoadFoldersAsync()
+        {
+            var folders = await m_ordnerService.GetFoldersAsync();
+            foreach (var folder in folders)
+            {
+                OrdnerCollection.Add(folder);
+            }
+        }
+
+        public async Task SaveFolderChangesAsync(Ordner folder)
+        {
+            if (!string.IsNullOrWhiteSpace(folder.Name))
+            {
+                await m_ordnerService.UpdateFolderAsync(folder); 
+            }
+        }
     }
 }
--- DokumentenManagementSystem/Ordneruebersicht/OrdnerFrame.xaml.cs
+++ DokumentenManagementSystem/Ordneruebersicht/OrdnerFrame.xaml.cs
@@ -1,28 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DMS.Model;
+using DMS.ViewModel.Ordneruebersicht;
 
 namespace DMS.View.Ordneruebersicht
 {
-    /// <summary>
-    /// Interaktionslogik für OrdnerFrame.xaml
-    /// </summary>
     public partial class OrdnerFrame : UserControl
     {
+        private TextBox? _lastFocusedTextBox;
+
         public OrdnerFrame()
         {
             InitializeComponent();
+
+            var viewModel = DataContext as OrdnerFrameVM;
+            if (viewModel != null)
+            {
+                viewModel.FolderCreated += OnFolderCreated;
+            }
+        }
+
+        private async void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox && textBox.DataContext is Ordner folder)
+            {
+                var viewModel = DataContext as OrdnerFrameVM;
+                if (viewModel != null)
+                {
+                    await viewModel.SaveFolderChangesAsync(folder);  
+                }
+
+                _lastFocusedTextBox = null;
+            }
+        }
+        private void OnFolderCreated(object sender, Ordner folder)
+        {
+            this.Dispatcher.InvokeAsync(() =>
+            {
+                foreach (var item in FolderItemsControl.Items)
+                {
+                    if (item is Ordner && item == folder)
+                    {
+                        var container = FolderItemsControl.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+
+                        if (container?.FindName("FolderNameTextBox") is TextBox textBox)
+                        {
+                            textBox.Focus();
+                            textBox.SelectAll();
+
+                            _lastFocusedTextBox = textBox;
+                        }
+                        break;
+                    }
+                }
+            });
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Keyboard.ClearFocus();
+        }
+
+        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (textBox == _lastFocusedTextBox && textBox.IsFocused)
+                    e.Handled = false;
+                else
+                {
+                    // If the TextBox is not focused, select all text
+                    textBox.Focus();
+                    textBox.SelectAll();
+                    _lastFocusedTextBox = textBox;
+                    e.Handled = true;  // Mark the event as handled to prevent double clicks
+                }
+            }
+        }
+
+        private void OnUserControlMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_lastFocusedTextBox != null)
+            {
+                Keyboard.ClearFocus(); 
+            }
         }
     }
+
 }
