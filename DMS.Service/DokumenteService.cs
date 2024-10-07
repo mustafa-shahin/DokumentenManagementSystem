759b00858c4221813db40609727a411dd37ad87a
Mustafa Shahin <mshahin@green-solutions.net>
2024-10-08 01:30:27 +0200

    upload and download files

--- DMS.Service/DokumenteService.cs
+++ DMS.Service/DokumenteService.cs
@@ -1,6 +1,78 @@
-﻿namespace DMS.Service;
+﻿using DMS.DataAccess;
+using DMS.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-public class DokumenteService
+namespace DMS.Service
 {
-    
+    public class DokumenteService
+    {
+        private readonly DataContext _context;
+
+        public DokumenteService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddFile(Ordner currentFolder, Benutzer currentUser)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var filePath = openFileDialog.FileName;
+                var fileContent = File.ReadAllBytes(filePath);
+
+                var dokument = new Dokument
+                {
+                    Name = Path.GetFileName(filePath),
+                    Description = "New file",
+                    Version = "1.0",
+                    Ersteller = currentUser,
+                    ErstellDatum = DateTime.Now,
+                    Content = fileContent,
+                    IsVisibleAllUser = false,
+                    OrdnerId = currentFolder.Id
+                };
+
+                dokument.Searchtags ??= [];
+                // Ensure the 'Ersteller' (Benutzer) is tracked, so EF doesn't try to insert a duplicate
+                if (_context.Benutzer.Any(b => b.Id == dokument.Ersteller.Id))
+                {
+                    // Attach the existing Benutzer to the context to avoid duplication
+                    _context.Entry(dokument.Ersteller).State = EntityState.Unchanged;
+                }
+                _context.Dokumente.Add(dokument);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateFile(Dokument dokument)
+        {
+            _context.Dokumente.Update(dokument);
+            _context.SaveChanges();
+        }
+
+        public async Task<List<Dokument>> GetFilesForFolder(int folderId, Benutzer user)
+        {
+            return await _context.Dokumente
+                .Where(d => d.OrdnerId == folderId && (d.IsVisibleAllUser || d.Ersteller.Id == user.Id))
+                .ToListAsync();
+        }
+
+        public static void DownloadFile(Dokument dokument)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                FileName = dokument.Name
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, dokument.Content);
+            }
+        }
+    }
 }
--- DMS.ViewModel/Dokumentenuebersicht/DokumentenView1VM.cs
+++ DMS.ViewModel/Dokumentenuebersicht/DokumentenView1VM.cs
@@ -1,4 +1,7 @@
-﻿using DMS.Model;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using DMS.Model;
+using DMS.Service;
 
 namespace DMS.ViewModel.Dokumentenuebersicht
 {
@@ -6,6 +9,14 @@ namespace DMS.ViewModel.Dokumentenuebersicht
     {
         private Ordner m_currentFolder;
         private Benutzer m_currentUser;
+        private readonly DokumenteService _dokumenteService;
+
+        public ObservableCollection<Dokument> FilesCollection { get; set; }
+
+        public ICommand AddFileCommand { get; set; }
+        public ICommand SaveFileCommand { get; set; }
+        public ICommand DownloadFileCommand { get; set; }
+
         public string FolderName
         {
             get => m_currentFolder?.Name;
@@ -18,11 +29,56 @@ namespace DMS.ViewModel.Dokumentenuebersicht
                 }
             }
         }
+
+        public DokumentenView1VM(DokumenteService dokumenteService)
+        {
+            _dokumenteService = dokumenteService;
+            AddFileCommand = new DelegateCommand(AddFile);
+            SaveFileCommand = new DelegateCommand(SaveFile);
+            DownloadFileCommand = new DelegateCommand(DownloadFile);
+            FilesCollection = [];
+        }
+
         public void Init(Ordner folder, Benutzer currentUser)
         {
             m_currentFolder = folder;
             m_currentUser = currentUser;
-            OnPropertyChanged(nameof(FolderName));
+            LoadFiles();
+        }
+
+        private async void LoadFiles()
+        {
+            var files = await _dokumenteService.GetFilesForFolder(m_currentFolder.Id, m_currentUser);
+            foreach (var file in files)
+            {
+                FilesCollection.Add(file);
+            }
+        }
+
+        private void AddFile(object obj)
+        {
+            // Call service to add file using OpenFileDialog
+            _dokumenteService.AddFile(m_currentFolder, m_currentUser);
+
+            // Refresh the FilesCollection after adding the file
+            FilesCollection.Clear();
+            LoadFiles();
+        }
+
+        private void SaveFile(object obj)
+        {
+            if (obj is Dokument file)
+            {
+                _dokumenteService.UpdateFile(file);
+            }
+        }
+
+        private void DownloadFile(object obj)
+        {
+            if (obj is Dokument file)
+            {
+                DokumenteService.DownloadFile(file);
+            }
         }
     }
 }
