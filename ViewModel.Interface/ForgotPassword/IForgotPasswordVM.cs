943291b4249a28145f441c2be0867fad1edc345e
Mustafa Shahin <mshahin@green-solutions.net>
2024-10-19 00:41:19 +0200

    Update IForgotPasswordVM.cs

--- ViewModel.Interface/ForgotPassword/IForgotPasswordVM.cs
+++ ViewModel.Interface/ForgotPassword/IForgotPasswordVM.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ViewModel.Interface.ForgotPassword
+﻿namespace ViewModel.Interface.ForgotPassword
 {
     public interface IForgotPasswordVM  : IViewModelBase
     {
