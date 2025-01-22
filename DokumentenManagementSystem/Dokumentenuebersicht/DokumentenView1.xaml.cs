2182c5de21721a61c01c74180261704e7c746bba
Mustafa Shahin <mustafashahin988@gmail.com>
2025-01-22 09:21:01 +0100

    null check

--- DokumentenManagementSystem/Dokumentenuebersicht/DokumentenView1.xaml.cs
+++ DokumentenManagementSystem/Dokumentenuebersicht/DokumentenView1.xaml.cs
@@ -148,7 +148,7 @@ namespace DMS.View.Dokumentenuebersicht
             var editDialog = new EditFileDialog(file, _viewModel.DokumenteService);
             var result = editDialog.ShowDialog();
 
-            if (result == true)
+            if (result)
             {
                 file.Name = editDialog.FileName;
                 file.Description = editDialog.FileDescription;
